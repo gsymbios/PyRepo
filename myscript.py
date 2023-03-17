2f6715ea180b3d0b772c5227b93ba2f0861432d1
gmanolakos <myemail@adomain.com>
2023-03-17 13:04:25 +0200

    Another minor change

--- myscript.py
+++ myscript.py
@@ -10,3 +10,4 @@ while True:
 print("The above is an infinite loop and this print will never happen")
 print("This is another line added to the master branch")
 print("This is another line added to the side branch")
+print("This is another line added to the side branch which I will send to the remote server")
